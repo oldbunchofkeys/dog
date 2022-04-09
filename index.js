35556766b6e4129c2ffabf4089cc536cad8e37a1
Pete Thompson <peter.thompson@roswellstudios.com>
2022-04-09 13:07:34 -0400

    fix whitespace and copy

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ newQuoteButton.addEventListener('click', getQuote);
 
 // const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
 const endpoint = 'https://dog.ceo/api/breeds/image/random';
+
 async function getQuote() {
     try {
         const response = await fetch(endpoint);
